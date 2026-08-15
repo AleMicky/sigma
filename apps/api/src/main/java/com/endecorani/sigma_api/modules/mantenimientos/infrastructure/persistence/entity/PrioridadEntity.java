354168b833031667ffb8d10bb6941dc276eeb27e
AleMicky <miguel.ale19934@gmail.com>
2026-08-15 14:53:45 -0400

    feat: define Prioridad domain model and persistence entity with auditing fields

--- apps/api/src/main/java/com/endecorani/sigma_api/modules/mantenimientos/infrastructure/persistence/entity/PrioridadEntity.java
+++ apps/api/src/main/java/com/endecorani/sigma_api/modules/mantenimientos/infrastructure/persistence/entity/PrioridadEntity.java
@@ -1,4 +1,28 @@
 package com.endecorani.sigma_api.modules.mantenimientos.infrastructure.persistence.entity;
 
-public class PrioridadEntity {
+import com.endecorani.sigma_api.shared.infrastructure.persistence.model.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Entity
+@Table(schema = "mantenimientos", name = "prioridades")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PrioridadEntity extends BaseEntity {
+    @Column(nullable = false, unique = true, length = 30)
+    private String codigo;
+
+    @Column(nullable = false, length = 100)
+    private String nombre;
+
+    @Column(length = 300)
+    private String descripcion;
+
+    @Column(nullable = false)
+    private Integer nivel;
 }
