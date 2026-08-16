f8bef083642d53a06b1c1ca036914121e93114e5
AleMicky <miguel.ale19934@gmail.com>
2026-08-16 19:10:26 -0400

    feat: implement Accesorio domain model and persistence entity with base class inheritance

--- apps/api/src/main/java/com/endecorani/sigma_api/modules/activos/infrastructure/persistence/entity/AccesorioEntity.java
+++ apps/api/src/main/java/com/endecorani/sigma_api/modules/activos/infrastructure/persistence/entity/AccesorioEntity.java
@@ -1,4 +1,41 @@
 package com.endecorani.sigma_api.modules.activos.infrastructure.persistence.entity;
 
-public class AccesorioEntity {
+import com.endecorani.sigma_api.shared.infrastructure.persistence.model.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(
+        schema = "activos",
+        name = "accesorios"
+)
+public class AccesorioEntity extends BaseEntity {
+
+    @Column(
+            name = "codigo",
+            nullable = false,
+            unique = true,
+            length = 50
+    )
+    private String codigo;
+
+    @Column(
+            name = "nombre",
+            nullable = false,
+            length = 100
+    )
+    private String nombre;
+
+    @Column(
+            name = "descripcion",
+            length = 255
+    )
+    private String descripcion;
 }
