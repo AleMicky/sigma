321790c0b259f103dc5c1fca633982618a4896a5
AleMicky <miguel.ale19934@gmail.com>
2026-08-16 19:04:42 -0400

    refactor: remove Accesorio domain model and add @Builder support to UnidadMedida

--- apps/api/src/main/java/com/endecorani/sigma_api/modules/activos/domain/model/Accesorio.java
+++
@@ -1,6 +0,0 @@
-package com.endecorani.sigma_api.modules.activos.domain.model;
-
-import com.endecorani.sigma_api.shared.domain.model.AuditableModel;
-
-public class Accesorio extends AuditableModel {
-}
