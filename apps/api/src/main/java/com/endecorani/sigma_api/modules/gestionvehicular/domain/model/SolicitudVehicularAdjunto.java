43b7d4b4597002e7fa7e952c2cd1815417a18a74
AleMicky <miguel.ale19934@gmail.com>
2026-09-26 15:37:41 -0400

    feat: implement SolicitudVehicularAdjunto entity and domain model

--- apps/api/src/main/java/com/endecorani/sigma_api/modules/gestionvehicular/domain/model/SolicitudVehicularAdjunto.java
+++ apps/api/src/main/java/com/endecorani/sigma_api/modules/gestionvehicular/domain/model/SolicitudVehicularAdjunto.java
@@ -1,4 +1,26 @@
 package com.endecorani.sigma_api.modules.gestionvehicular.domain.model;
 
-public class SolicitudVehicularAdjunto {
+import com.endecorani.sigma_api.shared.domain.model.AuditableModel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+public class SolicitudVehicularAdjunto extends AuditableModel {
+    private UUID id;
+    private UUID solicitudVehicularId;
+    private String nombreArchivo;
+    private String nombreOriginal;
+    private String url;
+    private String mimeType;
+    private Long size;
+    private String descripcion;
 }
